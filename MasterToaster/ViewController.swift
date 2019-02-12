50a25ee83ed0932eaea7bc366bde1279183a3e0d
Ash Cortez <ascortez@gyorgyhlen3.northamerica.corp.microsoft.com>
2019-02-12 11:14:35 -0800

    Initial Commit

---
+++ MasterToaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterToaster
+//
+//  Created by Ash Cortez on 2/12/19.
+//  Copyright © 2019 Turby_Turby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
